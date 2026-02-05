148b556218583bc44f3beba3c58284ce3f461c62
vothaihoangkhanh <vothaihoangkhanh234@gmail.com>
2026-02-05 20:01:44 +0700

    second commit

--- IdeaProjects/FX/src/main/java/Main8/loginController.java
+++ IdeaProjects/FX/src/main/java/Main8/loginController.java
@@ -1,9 +1,14 @@
 package Main8;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
+import javafx.stage.Stage;
 
 public class loginController {
 
@@ -14,9 +19,14 @@ public class loginController {
     private PasswordField txtPass;
 
     @FXML
+    private Label lblRegister;
+
+    @FXML
     public void initialize() {
-        limitInput(txtUser, 35);
+        limitInput(txtUser, 30);
         limitInput(txtPass, 16);
+
+        lblRegister.setOnMouseClicked(event -> openRegisterForm());
     }
 
     private void limitInput(TextField field, int maxLength) {
@@ -26,5 +36,23 @@ public class loginController {
             }
             return null;
         }));
+    }
+
+    private void openRegisterForm() {
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Signup.fxml"));
+            Parent root = loader.load();
+            Stage current = (Stage) lblRegister.getScene().getWindow();
+            current.close();
+
+            Stage stage = new Stage();
+            stage.setTitle("Đăng ký tài khoản");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
