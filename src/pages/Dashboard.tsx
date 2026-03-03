2a0baed6161f606779b7e2a8f277c16f42b45ee2
vothaihoangkhanh <vothaihoangkhanh234@gmail.com>
2026-03-03 17:39:55 +0700

    fixed Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ import { motion } from 'motion/react';
 
 const COLORS = ['#6366f1', '#f59e0b', '#10b981', '#ef4444'];
 
-export const Dashboard = () => {
+export const Dashboar = () => {
   const { tasks, projects, users } = useAppContext();
 
   const taskStats = [
